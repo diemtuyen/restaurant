2f4dd47af79b385fca525bbadd69d33d1da7349e
Diem Tuyen <lamdiemtuyen@gmail.com>
2018-10-29 17:21:53 +0700

    set background for status of table

--- client/src/components/presentation/TableListing.js
+++ client/src/components/presentation/TableListing.js
@@ -2,31 +2,15 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import {Button} from 'reactstrap';
-import { constants } from 'os';
-//import { StyleSheet, css } from 'aphrodite';
 class TableListing extends Component {
-    render() {
-        /*const styles = StyleSheet.create({ 
-            foo: { 
-                backgroundColor: `"#FF0000"`,
-                width: '200px',
-                border: `1px solid gray`,
-                textAlign: `center`,
-                padding: `10px`,
-                margin: `20px`,
-                marginRight: `10px`,
-                float: `left`
-            },
-          });*/
+    render() {        
         let bg = 'tableIdx'; 
         if(`${this.props.data.statusTable}` === `state_waiting`)
             bg = 'tableIdx wait';
         else if(`${this.props.data.statusTable}` === `state_billing`)
             bg = 'tableIdx bill';
         else if(`${this.props.data.statusTable}` === `state_order`)
-            bg = 'tableIdx order';
-        console.log (bg);
-        debugger;
+            bg = 'tableIdx order';        
         return (
             
             
@@ -36,7 +20,7 @@ class TableListing extends Component {
                 <br/>
                 <div>
                     <Button>Order</Button>{' '}
-                    <Button>Bill</Button>
+                    {(`${this.props.data.statusTable}` !== `state_order`) && <Button>Bill</Button>}
                 </div> 
             </div> 
         )
