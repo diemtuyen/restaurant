5dcc2d81a7b3297a625f9cf8a23e5626e03b4ebf
Diem Tuyen <lamdiemtuyen@gmail.com>
2018-12-07 08:56:01 +0700

    cleanup src, bk mongodb

---
+++ client/src/reducers/rootReducer.js
@@ -0,0 +1,16 @@
+import { combineReducers } from 'redux';
+import { reducer as reduxFormReducer } from 'redux-form';
+import { authentication } from './authentication.reducer';
+import { users } from './users.reducer';
+import loadingReducer from './loading.reducer';
+import {bookingReducer} from './booking.reducer';
+const rootReducer = combineReducers({
+    form:reduxFormReducer,
+    loading:loadingReducer,
+    authentication,
+    //users,
+    //tableReducer,
+    bookingReducer
+});
+
+export default rootReducer;
